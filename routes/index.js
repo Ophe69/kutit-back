673b018bd279f7f0a227fb26f22a491761628952
kontaba <60852537+kontaba@users.noreply.github.com>
2021-05-20 12:00:52 +0200

    get pro datas 2

--- routes/index.js
+++ routes/index.js
@@ -7,60 +7,41 @@ const ProfessionnelsModel = require('../models/professionnels');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-/* 
+
 //SIGN-IN
-router.post('/signin', function(req, res, next) {
-  let email = req.body.email;
-  let password = req.body.password;
-  if (!email || !password){
-    res.json({result: false})
-  } else {
-    res.json({ result: true, user: { pseudo: 'pim', firstName: 'Pierre',lastName: 'ferrand', email: 'pierre@gmail.com'} })
-  }
-  })
-  
+router.post('/sign-in', async function(req,res,next){
 
-//SIGNUP
-router.post('/sign-up', async function(req,res,next){
-
-  var error = [] //erreur dans un tableau n'est pas la meilleure solution 
   var result = false
-  var saveUser = null
-
-  const data = await userModel.findOne({
-    email: req.body.emailFromFront
-  })
-
-  if(data != null){
-    error.push('utilisateur déjà présent')
-  }
-
-  if(req.body.usernameFromFront == ''
-  || req.body.emailFromFront == ''
+  var user = null
+  var error = []
+  
+  if(req.body.emailFromFront == ''
   || req.body.passwordFromFront == ''
   ){
     error.push('champs vides')
   }
 
-
   if(error.length == 0){
-    var newUser = new userModel({
-      username: req.body.usernameFromFront,
+    const user = await userModel.findOne({
       email: req.body.emailFromFront,
       password: req.body.passwordFromFront
     })
-  
-    saveUser = await newUser.save()
-  
-    
-    if(saveUser){
+
+    if(user){
       result = true
+    } else {
+      error.push('email ou mot de passe incorrect')
     }
   }
-  
 
-  res.json({result, saveUser, error})
-}) */
+  res.json({result, user, error})
+
+})
+
+//signup
+router.post('/signup', (re, res) => {
+  res.json(true)
+})
 
 //Map
 router.post('/search', async(req, res) => {
@@ -75,6 +56,43 @@ router.post('/search', async(req, res) => {
   } else {
     res.json({ result: false, message: 'missing information, please enable geolocation' });
   }
-})
+});
+
+
+router.post('/prestations', async(req, res) => {
+  const prestations = await ProfessionnelsModel.findById(req.body.professionnel).populate('prestations').exec();
+  
+  res.render(prestations);
+});
+
+//create professional
+router.post('/create-pro', async(req, res) => {
+  const nom = req.body.nom;
+  const prenom = req.body.prenom;
+  const mail = req.body.mail;
+  const password = req.body.password;
+  const statut = req.body.statut;
+  const latitude = req.body.latitude;
+  const longitude = req.body.longitude;
+  const prestations = req.body.prestations;
+  
+  console.log(req.body)
+
+  const newPro = new ProfessionnelsModel({
+    nom: nom,
+    prenom: prenom,
+    mail: mail,
+    password: password,
+    statut: statut,
+    latitude: latitude,
+    longitude: longitude,
+    prestations: prestations
+  });
+  const proSaved = await newPro.save();
+
+  console.log(proSaved);
+
+  res.json({ result: true })
+});
 
 module.exports = router;
