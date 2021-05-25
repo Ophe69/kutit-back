b47be1892b51e93becc79afbc9ce8813b4f503b5
Ophelia Tagbo <ophelia.tagbo@gmail.com>
2021-05-25 11:02:09 +0200

    pull sur ophelia

--- routes/index.js
+++ routes/index.js
@@ -119,7 +119,45 @@ router.post('/search', async(req, res) => {
   } else {
     res.json({ result: false, message: 'missing information, please enable geolocation' });
   }
-})
+});
+
+
+
+router.post('/prestations', async(req, res) => {
+  const prestations = await ProfessionnelsModel.findById(req.body.professionnel).populate('prestations').exec();
+  
+  res.render(prestations);
+});
+
+//create professional
+router.post('/create-pro', async(req, res) => {
+  const nom = req.body.nom;
+  const prenom = req.body.prenom;
+  const mail = req.body.mail;
+  const password = req.body.password;
+  const statut = req.body.statut;
+  const latitude = req.body.latitude;
+  const longitude = req.body.longitude;
+  const prestations = req.body.prestations;
+  
+  console.log(req.body)
+
+  const newPro = new ProfessionnelsModel({
+    nom: nom,
+    prenom: prenom,
+    mail: mail,
+    password: password,
+    statut: statut,
+    latitude: latitude,
+    longitude: longitude,
+    prestations: prestations
+  });
+  const proSaved = await newPro.save();
+
+  console.log(proSaved);
+
+  res.json({ result: true })
+});
 
 
 
